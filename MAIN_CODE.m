8a99a482c15a730acb2d244eee5bf55d2d1e6a91
pSQuar3 <pratyaypande@gmail.com>
2021-01-14 11:18:28 +0530

    Final Commit before presentation

--- MAIN_CODE.m
+++ MAIN_CODE.m
@@ -1,17 +1,12 @@
-%take the rate matrix
 rates = sinc_dist(20,1,pi);
 n = size(rates,2);
 figure
-c = 1:1:n;
-scatter(c,rates);
-title("Rate of growth across the leaf");
-xlabel("Compartment Number");
+c = 1:1:n; scatter(c,rates);
+title("Rate of growth across the leaf"); xlabel("Compartment Number");
 ylabel("Rate of cell division in compartment");
 cell_number_limit = 20;
 radius = 0.001;
-sigma = 0.001;
-%given the rates, obtain the files from get_data
-i = 1;
+sigma = 0.001; i = 1;
 while(i <= n)
     fprintf("Recorded data of compartment %d in values_%d.csv\n",i,i);
     gt = get_data(rates(i),cell_number_limit,i,radius,sigma);
@@ -23,9 +18,7 @@ while(i <= n)
     nme = 'values_' + string(i) + '.csv';
     dpf = readmatrix('cell_data\' + nme);
     de = size(dpf,1);
-    A(i,1) = dpf(de,1);
-    A(i,2) = dpf(de,2);
-    A(i,3) = dpf(de,3);
+    A(i,1) = dpf(de,1); A(i,2) = dpf(de,2); A(i,3) = dpf(de,3);
     i = i + 1;
 end
 b = zeros(n,1);
@@ -35,17 +28,9 @@ dx = 1;
 % final plot of the data
 gg = n-1;
 for i = 1:n
-    radius = A(i,1);
-    sep = A(i,2);
-    num = A(i,3);
+    radius = A(i,1); sep = A(i,2); num = A(i,3);
     x = 0:sep:((num-1)*sep);
-    %fprintf("radius = %f, separation = %f, number of cells = %d\n",radius,sep,n);
     axis manual;
-    %axis([-1 cell_number_limit*10000 -1 2]);
     scatter(x,ones(1,num) + gg,2*radius,'k');
-    xlim([0 600]);
-    gg = gg - 1;
-    hold on;
+    xlim([0 600]); gg = gg - 1; hold on;
 end
-
-
